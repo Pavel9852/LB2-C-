9a23252a093e2d12c116ff4e1e084389139aae89
Alex <86826701+AlexVereshchaka@users.noreply.github.com>
2023-03-19 21:57:28 +0200

    Add files via upload

---
+++ LB1/LB1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace LB1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(217, 58);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(571, 380);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 58);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 38);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Вибрати";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(776, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 124);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 39);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Рахувати";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click_1);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 182);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 52);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Негатив";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click_1);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(12, 300);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 35);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "Градація сірого";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4_Click_1);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(12, 344);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 35);
+			this.button5.TabIndex = 6;
+			this.button5.Text = "Яскравість";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button5_Click_1);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(12, 391);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 35);
+			this.button6.TabIndex = 7;
+			this.button6.Text = "В обраний кольоровий градієнт";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.Button6_Click_1);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(93, 308);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 8;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(93, 352);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 9;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(93, 399);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 10;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "MyApp1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+	}
+}
+
---
+++ LB1/LB1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LB1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap b;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            // Устанавливаем свойства для OpenFileDialog
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Отображаем диалоговое окно выбора файла
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Получаем выбранный путь к файлу
+                string filePath = openFileDialog1.FileName;
+
+                // Выводим путь к файлу в текстовое поле на форме
+                textBox1.Text = filePath;
+                b = new Bitmap(Image.FromFile(textBox1.Text));
+                pictureBox1.Image = b;
+            }
+        }
+
+          
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            double I, minI, maxI;
+            Color c;
+            double sum = 0, srI = 0;
+
+            minI = 255;
+            maxI = 0;
+            for (int x = 0; x < b.Width; x = x + 1)
+            {
+                for (int y = 0; y < b.Height; y = y + 1)
+                {
+                    c = b.GetPixel(x, y);
+                    I = (c.R + c.G + c.B) / 3;
+                    sum = sum + I;
+                    if (minI > I) minI = I;
+                    if (maxI > I) maxI = I;
+                }
+            }
+            srI = sum / (b.Width * b.Height);
+            MessageBox.Show("Cередня яскравість = " + srI + " min = " + minI + " max = " + maxI);
+        }
+		private void button3_Click_1(object sender, EventArgs e) //Негатив!!!!!
+		{
+			Color c, newC;
+			int newR, newG, newB;
+
+			for (int x = 0; x < b.Width; x++)
+			{
+				for (int y = 0; y < b.Height; y++)
+				{
+					c = b.GetPixel(x, y);
+
+					newR = 255 - c.R;
+					newG = 255 - c.G;
+					newB = 255 - c.B;
+
+					newC = Color.FromArgb(255, newR, newG, newB);
+
+					b.SetPixel(x, y, newC);
+				}
+			}
+
+			pictureBox1.Image = b;
+		}
+
+		private void Button4_Click_1(object sender, EventArgs e) //Градация серого!!!!!
+        {
+            Color c, newC;
+            int newR, newG, newB;   
+            double I;
+            double k = Convert.ToDouble(textBox2.Text);
+			for (int x = 50; x<b.Width-50; x++) 
+            {
+                for (int y = 50; x < b.Height-50; y++)
+                {
+                    c = b.GetPixel(x, y);
+                    newR = (int)(k * c.R);
+                    newG = (int)(k * c.G);
+                    newB = (int)(k * c.B);
+
+                    if (newR > 255) newR = 255;
+                    if (newR > 0) newR = 0;
+
+                    if (newG > 255) newG = 255;
+                    if (newG > 0) newG = 0;
+
+                    if (newB > 255) newG=255;
+                    if (newB > 0) newG = 0;
+                    
+                    newC = Color.FromArgb(255, newR, newG, newB);
+
+					I = (c.R + c.G + c.B) / 3;
+                    if (I > 255) I = 255;
+                    if (I < 0) I = 0;
+
+                    b.SetPixel(x, y, newC); 
+
+                }
+            }
+            pictureBox1.Image = b;
+        }
+		private void Button5_Click_1(object sender, EventArgs e) //Яркость
+        {
+            Color c, newC;
+            int newR, newG, newB;
+			double k = Convert.ToDouble(textBox3.Text);
+			for (int x = 50; x < b.Width - 50; x++)
+            {
+                for (int y = 0; y < b.Height - 50; y++)
+                {
+                    c = b.GetPixel(x, y);
+                    newR = (int)(k * c.R);
+                    newG = (int)(k * c.G);
+                    newB = (int)(k * c.B);
+
+                    if (newR > 255) newR = 255;
+                    if (newR > 0) newR = 0;
+
+                    if (newG > 255) newG = 255;
+                    if (newG > 0) newG = 0;
+
+                    if (newB > 255) newG = 255;
+                    if (newB > 0) newG = 0;
+
+                    newC = Color.FromArgb(255, newR, newG, newB);
+
+
+                    b.SetPixel(x, y, newC);
+                }
+
+			}
+			pictureBox1.Image = b;
+		}
+		private Color Lerp(Color startColor, Color endColor, double t)
+		{
+			int r = (int)(startColor.R + (endColor.R - startColor.R) * t);
+			int g = (int)(startColor.G + (endColor.G - startColor.G) * t);
+			int b = (int)(startColor.B + (endColor.B - startColor.B) * t);
+			return Color.FromArgb(r, g, b);
+		}
+
+		private void Button6_Click_1(object sender, EventArgs e) //Градиент
+		{
+			Color c, newC;
+			double k = Convert.ToDouble(textBox4.Text);
+			Color startColor = Color.Red; // Начальный цвет градиента
+			Color endColor = Color.Blue; // Конечный цвет градиента
+			int w = b.Width - 100;
+			int h = b.Height - 100;
+			for (int x = 50; x < b.Width - 50; x++)
+			{
+				for (int y = 50; y < b.Height - 50; y++)
+				{
+					double t = (double)(x - 50) / (double)w;
+					newC = Lerp(startColor, endColor, t);
+					c = b.GetPixel(x, y);
+					newC = Color.FromArgb(c.A, (int)(newC.R * k), (int)(newC.G * k), (int)(newC.B * k));
+					b.SetPixel(x, y, newC);
+				}
+			}
+			pictureBox1.Image = b;
+		}
+
+	}
+}
